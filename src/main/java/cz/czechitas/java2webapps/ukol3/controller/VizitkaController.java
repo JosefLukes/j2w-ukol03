23445deba17d6fcfec2d4132a618f0213f6296ec
admin <@>
2021-04-22 06:38:37 +0200

    VIZITKA CLASS

--- src/main/java/cz/czechitas/java2webapps/ukol3/controller/VizitkaController.java
+++ src/main/java/cz/czechitas/java2webapps/ukol3/controller/VizitkaController.java
@@ -2,6 +2,7 @@ package cz.czechitas.java2webapps.ukol3.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Kontroler obsluhující zobrazování vizitek.
@@ -9,12 +10,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class VizitkaController {
   @GetMapping("/")
-  public String seznam() {
-    return "seznam";
+  public ModelAndView seznam() {
+    ModelAndView result = new ModelAndView("seznam");
+
+    return result;
   }
 
   @GetMapping("/detail")
-  public String detail() {
-    return "detail";
+  public ModelAndView detail() {
+    ModelAndView result = new ModelAndView("detail");
+
+    return result;
   }
 }
